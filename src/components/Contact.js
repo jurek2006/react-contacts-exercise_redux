45e458173f709c9c56f9a1ee7e6e829fa6f4d90e
Jurek <jurek2006@gmail.com>
2018-09-26 10:45:12 +0200

    Usuwanie poszczególnych kontaktów - dodanie reducera i akcji w Context

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import { Consumer } from "../context";
 
 export class Contact extends Component {
+    handleDeleteContact(id, dispatch) {
+        dispatch({ type: "DELETE_CONTACT", payload: id });
+    }
+
     render() {
-        const { firstName, lastName, email, phone, city } = this.props.contact;
+        const {
+            firstName,
+            lastName,
+            email,
+            phone,
+            city,
+            id
+        } = this.props.contact;
         return (
-            <li>
-                {firstName} {lastName} {email} {phone} {city}{" "}
-                <i className="fas fa-times" />
-            </li>
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <li>
+                            {firstName} {lastName} {email} {phone} {city}{" "}
+                            <i
+                                className="fas fa-times"
+                                onClick={this.handleDeleteContact.bind(
+                                    this,
+                                    id,
+                                    dispatch
+                                )}
+                            />
+                        </li>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
--- src/context.js
+++ src/context.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import uuid from "uuid";
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "DELETE_CONTACT":
+            return {
+                ...state,
+                contacts: state.contacts.filter(
+                    contact => contact.id !== action.payload
+                )
+            };
+        default:
+            return state;
+    }
+};
+
 export class Provider extends Component {
     state = {
         contacts: [
@@ -21,7 +35,8 @@ export class Provider extends Component {
                 city: "Łękołoty Wielkie",
                 id: uuid()
             }
-        ]
+        ],
+        dispatch: action => this.setState(state => reducer(state, action))
     };
 
     render() {
