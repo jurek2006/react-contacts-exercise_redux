0a3f68a306445478e920ffd2c6aaf3aaece21ee5
Jurek <jurek2006@gmail.com>
2018-09-26 10:09:51 +0200

    Dodanie fontAwesome

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,8 @@ export class Contact extends Component {
         const { firstName, lastName, email, phone, city } = this.props.contact;
         return (
             <li>
-                {firstName} {lastName} {email} {phone} {city}
+                {firstName} {lastName} {email} {phone} {city}{" "}
+                <i className="fas fa-times" />
             </li>
         );
     }
